699099d44793743cb39f60c3279c4fd76b0cd282
Roshan R <roshanrajeev2001@gmail.com>
2024-12-12 15:07:53 +0530

    JWT authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls.conf import include, path
-from .views import AccountCreateView
+from .views import AccountCreateView, AccountListView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("auth/register", AccountCreateView.as_view(), name="register"),
+    path("auth/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("users", AccountListView.as_view(), name="users_list"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 from rest_framework import status
 from .services import account_create
 from api.errors import ApiErrorsMixin
+from api.mixins import ApiAuthMixin
 from .models import Account
 from rest_framework.validators import UniqueValidator
 # Create your views here.
@@ -21,3 +22,15 @@ class AccountCreateView(ApiErrorsMixin, APIView):
         user = account_create(**serializer.validated_data)
         return Response(status=status.HTTP_201_CREATED)
 
+class AccountListView(ApiErrorsMixin, ApiAuthMixin, APIView):
+    class OutputSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Account
+            fields = ("id", "email", "first_name", "last_name")
+
+    def get(self, request):
+        # accounts = accounts_list(filters=filter_seriaizer.validated_data)
+        accounts = Account.objects.all()
+
+        data = self.OutputSerializer(instance=accounts, many=True).data
+        return Response(data=data)
