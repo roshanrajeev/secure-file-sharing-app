912b3ca9024bd2312fa50e9dcfcdd187dfaef402
Roshan R <roshanrajeev2001@gmail.com>
2024-12-12 18:52:00 +0530

    List and download files

--- storage/urls.py
+++ storage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import FolderCreateView, FileUploadView
+from .views import FolderCreateView, FileUploadView, FileListView, FolderDownloadView
 
 urlpatterns = [
     path("folders/create", FolderCreateView.as_view(), name="create_folder"),
-    path("files/upload", FileUploadView.as_view(), name="upload_file"),
+    path("folders/<str:folder_uid>/files/upload", FileUploadView.as_view(), name="upload_file"),
+    path("folders/<str:folder_uid>/files", FileListView.as_view(), name="list_files"),
+    path("folders/<str:folder_uid>/files/download", FolderDownloadView.as_view(), name="download_folder"),
 ]
--- storage/views.py
+++ storage/views.py
@@ -8,6 +8,9 @@ from api.mixins import ApiAuthMixin
 from .models import Folder, File
 from rest_framework.parsers import FormParser, MultiPartParser
 from .services import create_file, create_folder
+from django.http import Http404
+from django.core.files.storage import default_storage
+from django.http import FileResponse
 
 # Create your views here.
 class FileUploadView(ApiErrorsMixin, ApiAuthMixin, APIView):
@@ -15,12 +18,11 @@ class FileUploadView(ApiErrorsMixin, ApiAuthMixin, APIView):
 
     class InputSerializer(serializers.Serializer):
         file = serializers.FileField(required=True)
-        folder_uid = serializers.UUIDField(required=True)
 
-    def post(self, request):
+    def post(self, request, folder_uid):
         serializer = self.InputSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        create_file(**serializer.validated_data)
+        create_file(folder_uid=folder_uid, **serializer.validated_data)
         return Response(status=status.HTTP_200_OK)
 
 class FolderCreateView(ApiErrorsMixin, ApiAuthMixin, APIView):
@@ -33,3 +35,41 @@ class FolderCreateView(ApiErrorsMixin, ApiAuthMixin, APIView):
         folder = create_folder()
         data = self.OutputSerializer(instance=folder).data
         return Response(data, status=status.HTTP_201_CREATED)
+
+class FileListView(ApiErrorsMixin, ApiAuthMixin, APIView):
+    class OutputSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = File
+            fields = ["uid", "name", "created_at"]
+
+    def get(self, request, folder_uid):
+        files = File.objects.filter(folder__uid=folder_uid)
+        data = self.OutputSerializer(instance=files, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+
+class FolderDownloadView(ApiErrorsMixin, ApiAuthMixin, APIView):
+    def get(self, request, folder_uid):
+        folder = Folder.objects.get(uid=folder_uid)
+        files = folder.files.all()
+
+        if len(files) == 1:
+            # Return a single file directly
+            file_obj = files[0].file
+            file_path = default_storage.path(file_obj.name)
+            return FileResponse(open(file_path, "rb"), as_attachment=True, filename=file_obj.name.split("/")[-1])
+        
+        elif len(files) > 1:
+            # Generate a zip archive
+            import zipfile
+            from io import BytesIO
+
+            zip_buffer = BytesIO()
+            with zipfile.ZipFile(zip_buffer, "w") as zip_file:
+                for file_obj in files:
+                    file_path = default_storage.path(file_obj.file.name)
+                    zip_file.write(file_path, arcname=file_obj.file.name.split("/")[-1])
+            zip_buffer.seek(0)
+
+            return FileResponse(zip_buffer, as_attachment=True, filename=f"{folder_uid}_files.zip")
+
+        raise Http404("No files found in the folder.")
