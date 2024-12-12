03d9aa9eb6ddc34b7c775288b8c3a59a33fb1d78
Roshan R <roshanrajeev2001@gmail.com>
2024-12-12 19:24:39 +0530

    Renamed to backend

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for file_share_app project.
+WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'file_share_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
